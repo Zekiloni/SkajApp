f014ae09a4fd4a5232c00d08d5ebdec67ad967fc
Zekiloni <zekirija17@gmail.com>
2024-11-17 22:28:37 +0100

    Implemented Guid utils, parse from string to Guid

---
+++ Server/Infrastructure/Utilities/GuidUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.Infrastructure.Utilities
+{
+    public class GuidUtils
+    {
+        public static Guid Parse(string value)
+        {
+            if (value == null)
+                return Guid.Empty;
+
+            return Guid.Parse(value);
+        }
+    }
+}
